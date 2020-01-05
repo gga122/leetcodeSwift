ad41dff0aaa52d09e8cbcb401a73f47255d691fb
henry <zhy289044312@gmail.com>
2020-01-06 00:59:26 +0800

    [comment] 增加DP说明

--- leetcode/leetcode/LongestPalindromicSubsequence.swift
+++ leetcode/leetcode/LongestPalindromicSubsequence.swift
@@ -13,6 +13,12 @@ class LongestPalindromicSubsequenceSolution {
         let chars = Array(s)
         var dp = [[Int]].init(repeating: [Int].init(repeating: 0, count: chars.count), count: chars.count)
 
+        /*
+         dp[i][j] -> [i, j]的最大子序列长度
+         chars[i] == chars[j] 那么 dp[i][j] = dp[i+1][j-1] + 2 (前一个最大长度状态 + 2)
+         chars[i] != chars[j] 那么 dp[i][j] = max(dp[i+1][j], dp[i][j-1]) (右边子序列最大长度状态, 左边子序列最大长度状态)
+         dp[i][i] = 1 自身最大长度一直是1
+         */
         for i in (0..<chars.count).reversed() {
             dp[i][i] = 1
             for j in i+1..<chars.count {
