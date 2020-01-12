95b3a590b007e615ef1ec3a7bb929b4160c4311f
Henry <zhy289044312@gmail.com>
2020-01-13 01:28:32 +0800

    [improve] 解决或运算的最小翻转次数问题（5308）

--- leetcode/leetcode/main.swift
+++ leetcode/leetcode/main.swift
@@ -400,44 +400,7 @@ CapacityToShipPackagesWithinDDaysSolution.init().test()
 /* DP Test */
 //BasicPackageSolution.init().test()
 
-//class Solution {
-//    func getNoZeroIntegers(_ n: Int) -> [Int] {
-//        var target = 0
-//        for i in 1...n/2 {
-//            if containZero(i) {
-//                continue
-//            }
-//            if containZero(n-i) {
-//                continue
-//            }
-//            target = i
-//            break
-//        }
-//
-//        return [target, n-target]
-//    }
-//
-//    func containZero(_ num: Int) -> Bool {
-//        if num == 0 {
-//            return true
-//        }
-//        var num = num
-//        while num > 0 {
-//            if num % 10 == 0 {
-//                return true
-//            }
-//            num = num / 10
-//        }
-//
-//        return false
-//    }
-//
-//    func test() -> Void {
-//        print(getNoZeroIntegers(2))
-//        print(getNoZeroIntegers(11))
-//        print(getNoZeroIntegers(10000))
-//    }
-//}
+
 
 //class Solution {
 //    func minFlips(_ a: Int, _ b: Int, _ c: Int) -> Int {
