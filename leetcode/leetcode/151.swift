2eb3c1418a83982dbbb3147d23b51eb2a6f35d77
Henry <zhy289044312@gmail.com>
2022-08-24 23:52:12 +0800

    [improve] 使用原地算法解决leetcode151

--- leetcode/leetcode/151.swift
+++ leetcode/leetcode/151.swift
@@ -10,6 +10,57 @@ import Foundation
 
 class Solution151 {
     func reverseWords(_ s: String) -> String {
+        var chars = [Character].init(s)
+
+        /* Remove prefix and suffix empty characters */
+        while chars.first == " " {
+            chars.removeFirst()
+        }
+        chars.reverse()
+        while chars.first == " " {
+            chars.removeFirst()
+        }
+        
+        /* Remove middle empty characters */
+        var indexes = [Int]()
+        for i in chars.startIndex..<chars.endIndex {
+            if chars[i] != " "  {
+                continue
+            }
+            
+            if i - 1 >= chars.startIndex && chars[i-1] == " " {
+                indexes.append(i)
+            }
+        }
+        for i in indexes.reversed() {
+            chars.remove(at: i)
+        }
+        
+        /* Reverse each word */
+        chars.append(" ")
+        var head = chars.startIndex
+        var tail = chars.startIndex
+        while tail < chars.endIndex {
+            if chars[tail] == " " {
+                var end = tail - 1
+                while head < end {
+                    chars.swapAt(head, end)
+                    head += 1
+                    end -= 1
+                }
+                
+                head = tail + 1
+            }
+            
+            tail += 1
+        }
+        chars.removeLast()
+        
+        let dst = String.init(chars)
+        return dst
+    }
+    
+    func reverseWords1(_ s: String) -> String {
         var components = s.components(separatedBy: " ")
         components = components.filter { comp in
             return comp != ""
