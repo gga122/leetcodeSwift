cc159897def35392ed5b92dd0f1752e96c4c2354
John Henry <johnxhenry@foxmail.com>
2024-11-17 17:38:28 +0800

    solved: 解决problem2239

--- leetcode2024/LeetCode/LeetCode/main.swift
+++ leetcode2024/LeetCode/LeetCode/main.swift
@@ -18,7 +18,8 @@ import Foundation
 //print(Solution_55.init().canJump([0,2,3]))
 //print(Solution_45.init().jump([1,2]))
 //print(Solution_1137.init().tribonacci(25))
-print(Solution_3258.init().countKConstraintSubstrings("10101", 1))
+//print(Solution_3258.init().countKConstraintSubstrings("10101", 1))
+print(Solution_2239.init().findClosestNumber([-4,-2,1,4,8]))
 
 //Solution_88.init().test()
 //Solution_27.init().test()
